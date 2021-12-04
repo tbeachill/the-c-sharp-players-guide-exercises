0c2a509902229ae1f31290c51e874ad0b2dac244
tbeachill <82418020+tbeachill@users.noreply.github.com>
2021-12-04 19:11:31 +0000

    finished exercise

---
+++ Comments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); // type of object
+
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); // description of object
+
+string c = "of Doom"; // appended description
+string d = "3000";
+
+Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!");
---
+++ ConsoleReadWrite/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+
+string name;
+name = Console.ReadLine();
+
+Console.WriteLine("Noted: " + name + " got bread.");
