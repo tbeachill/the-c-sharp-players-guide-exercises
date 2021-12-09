2dc81672683ec4d67b2310daf511e36a1ef68e21
tbeachill <82418020+tbeachill@users.noreply.github.com>
2021-12-09 22:35:42 +0000

    finished exercise

---
+++ SimulasTest/Program.cs
@@ -0,0 +1,48 @@
+﻿ChestState currentState = ChestState.Locked;
+ChangeState(currentState);
+
+ChestState ChangeState(ChestState currentState)
+{
+    while (true)
+    {
+        Console.Write($"The chest is {currentState}. What do you want to do? ");
+        string userIntent = Console.ReadLine();
+
+        switch (userIntent)
+        {
+            case "close":
+                if (currentState == ChestState.Open)
+                    currentState = ChestState.Closed;
+                else
+                    Console.WriteLine("You can't do that.");
+                break;
+
+            case "lock":
+                if (currentState == ChestState.Closed)
+                    currentState = ChestState.Locked;
+                else
+                    Console.WriteLine("You can't do that.");
+                break;
+
+            case "unlock":
+                if (currentState == ChestState.Locked)
+                    currentState = ChestState.Closed;
+                else
+                    Console.WriteLine("You can't do that.");
+                break;
+
+            case "open":
+                if (currentState == ChestState.Closed)
+                    currentState = ChestState.Open;
+                else
+                    Console.WriteLine("You can't do that.");
+                break;
+
+            default:
+                Console.WriteLine("I don't understand.");
+                break;
+        }
+    }
+}
+
+enum ChestState { Open, Closed, Locked };
