c2f6bd85d58ec7df2b09719c2fb59e1cf8ad8c7c
tbeachill <82418020+tbeachill@users.noreply.github.com>
2021-12-23 18:51:56 +0000

    finished exercise

---
+++ EnteringTheCatacombs/Program.cs
@@ -0,0 +1,22 @@
+﻿Point pointOne = new Point(2, 3);
+Point pointTwo = new Point(-4, 0);
+
+Console.WriteLine("Point 1: (" + pointOne.x + "," + pointOne.y + ")");
+Console.WriteLine("Point 2: (" + pointTwo.x + "," + pointTwo.y + ")");
+public class Point
+{
+    public float x { get; set; }
+    public float y { get; set; }
+
+    public Point(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point()
+    {
+        this.x = 0;
+        this.y = 0;
+    }
+}
