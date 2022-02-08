8fd92b6803a899c950ac96129fa14dcb8a04cab3
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-02-08 14:25:22 +0000

    picks a random word from a txt file

---
+++ Hangman/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine(Dictionary.GetWord());
+
+static class Dictionary
+{
+    public static string GetWord()
+    {
+        string[] words = File.ReadAllLines(@"C:\Users\Tom\OneDrive\Projects\Learning\The_C_Sharp_Players_Guide\Hangman\words.txt");
+        Random rnd = new Random();
+        int num = rnd.Next(0, words.Length);
+
+        return words[num];
+
+    }
+}
+
+class Player
+{
+    // Knows word, guesses and incorrect letters
+}
+
+class GameRunner
+{
+    // Pick a letter
+    // State of game
+}
