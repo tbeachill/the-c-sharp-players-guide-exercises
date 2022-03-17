9e85c7f8e8ca04e34fa503078ec949f6816d08bb
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-03-17 22:52:01 +0000

    finished exercise

---
+++ WarPreparations/Program.cs
@@ -0,0 +1,13 @@
+﻿// create three sword instances and write out each sword
+
+Sword swordA = new Sword(Material.Iron, Gemstone.None, 5, 1);
+Sword swordB = swordA with { material = Material.Bronze };
+Sword swordC = swordA with { gemstone = Gemstone.Amber, length = 4.5f };
+
+Console.WriteLine(swordA + "\n" + swordB + "\n" + swordC);
+
+
+public record Sword(Material material, Gemstone gemstone, float length, float crossguardWidth);
+
+public enum Material { Wood, Bronze, Iron, Steel, Binarium };
+public enum Gemstone { None, Emerald, Amber, Sapphire, Diamond, Bitstone };
