62e8e5e54b6e8e25c330aaec57441641a58f78ba
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-03-20 17:15:10 +0000

    finished exercise

---
+++ SaferNumberCrunching/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter a value: ");
+
+if (int.TryParse(Console.ReadLine(), out int intValue))
+{
+    Console.WriteLine($"Int entered: {intValue}.");
+}
+else if (double.TryParse(Console.ReadLine(),out double doubleValue))
+{
+    Console.WriteLine($"Double entered: {doubleValue}.");
+}
+else if (bool.TryParse(Console.ReadLine(), out bool boolValue))
+{
+    Console.WriteLine($"Bool entered: {boolValue}.");
+}
+else
+{
+    Console.WriteLine("Input not int, double or bool.");
+}
