21b6abf259f2281c1907c941c039c97c16200112
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-03-20 16:21:07 +0000

    finished exercise

---
+++ TheFeud/Program.cs
@@ -0,0 +1,19 @@
+﻿using Flame;
+using Frost;
+
+Sheep sheep = new Sheep();
+Cow cow = new Cow();
+Flame.Pig flamePig = new Flame.Pig();
+Frost.Pig frostPig = new Frost.Pig();
+
+namespace Flame
+{
+    public class Sheep { }
+    public class Pig { }
+}
+
+namespace Frost
+{
+    public class Cow { }
+    public class Pig { }
+}
