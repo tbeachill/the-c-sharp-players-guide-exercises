32adebc4fc333c01b8936235b1aab3c41ba66d7f
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-03-24 21:39:40 +0000

    finished exercise

---
+++ ExceptisGame/Program.cs
@@ -0,0 +1,32 @@
+﻿Random r = new Random();
+int oatmealCookie = r.Next(0, 9);
+List<int> selectionList = new List<int> { };
+
+while (true)
+{
+    try { MakeSelection(); }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+        Console.WriteLine("You lose.");
+    }
+}
+
+
+void MakeSelection()
+{
+    // make a selection, check if it is oatmeal, then add selection to the selection list
+
+    int selection;
+    do
+    {
+        Console.Write($"Player {selectionList.Count % 2 + 1}: ");
+        int.TryParse(Console.ReadLine(), out selection);
+    }
+    while (selectionList.Contains(selection));
+
+    if (selection == oatmealCookie)
+        throw new Exception("Oatmeal");
+
+    selectionList.Add(selection);
+}
