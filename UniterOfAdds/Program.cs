36a5eb4eea6d326d3d5f9aecc87bfb3f04a25bfa
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-03-28 18:24:27 +0100

    finished exercise

---
+++ UniterOfAdds/Program.cs
@@ -0,0 +1,8 @@
+﻿// a single dynamic method can can add different types
+
+Console.WriteLine(Add(2, 2));
+Console.WriteLine(Add(1.25, 1.25));
+Console.WriteLine(Add("123", "456"));
+Console.WriteLine(Add(DateTime.Now, TimeSpan.FromDays(1)));
+
+dynamic Add(dynamic a, dynamic b) => a + b;
