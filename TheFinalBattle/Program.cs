bb8b8bde863701c701d7fc4fb351ce9a7fe0a54f
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-03-29 21:11:25 +0100

    added true programmer char

--- TheFinalBattle/Program.cs
+++ TheFinalBattle/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main()
         {
-            Party heroes = new Party(new Skeleton());
+            Party heroes = new Party(new TrueProgrammer());
             Party monsters = new Party(new Skeleton());
 
             Battle battle = new Battle(heroes, monsters);
---
+++ TheFinalBattle/TrueProgrammer.cs
@@ -0,0 +1,12 @@
+﻿namespace TheFinalBattle
+{
+    internal class TrueProgrammer : Character
+    {
+        public override string Name { get; set; }
+        public TrueProgrammer()
+        {
+            Console.WriteLine("What is your name? ");
+            Name = Console.ReadLine();
+        }
+    }
+}
