85d760cf6895c59a49c168ab30e21d23e55147ad
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-03-29 16:32:39 +0100

    finished exercise

---
+++ ColoredConsole/Program.cs
@@ -0,0 +1,4 @@
+﻿using ConsoleLibrary;
+
+string name = ColoredConsole.Prompt("What is your name?");
+ColoredConsole.WriteLine("Hello " + name, ConsoleColor.Green);
---
+++ ConsoleLibrary/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleLibrary
+{
+    public class ColoredConsole
+    {
+        public static string Prompt(string question)
+        {
+            Console.Write(question + " ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string answer = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            return answer;
+        }
+
+        public static void WriteLine(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+
+            return;
+        }
+
+        public static void Write(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = ConsoleColor.White;
+
+            return;
+        }
+    }
+}
